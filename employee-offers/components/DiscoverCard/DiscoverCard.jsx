b577a22e5d9b56f49389ae64df0be81b9061f92d
Pankaj Pal <pankaj.pal@ayasya.com>
2025-11-11 18:19:16 +0530

    update card

---
+++ employee-offers/components/DiscoverCard/DiscoverCard.jsx
@@ -0,0 +1,35 @@
+const Discover = () => {
+  return (
+    <div className="grid grid-cols-2 items-center justify-between">
+      <div className="">
+        <div>
+          <h1 className="text-left mb-4 text-4xl text-black font-medium">
+            Discover Conveniance Like Never Before..
+          </h1>
+          <p className="text-md text-[#333333cc] w-[70%]">
+            Unlock financial tools, Investment insights, And Expert Guidance -
+            All in one convenient app
+          </p>
+        </div>
+        <div className="mt-[58px] ">
+          <h3 className="text-2xl ">Download the ABCD App Now</h3>
+          <div className="flex  mt-4">
+            <a
+              href="/#"
+              className="w-[184px] flex gap-2.5 items-center justify-center bg-[#CA1F34] text-white  rounded-full text-[16px] leading-[18px] py-4"
+            >
+              Download App <img src="/images/play-store.png" alt="img" />{" "}
+              <img src="/images/dwn-app-store.png" alt="img2" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="animate-custom-bounce">
+        <img src="/images/abg_discover_download_img.png" alt="img3"/>
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
+ 
