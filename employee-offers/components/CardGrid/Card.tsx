4357a4d36b83f4c582cf41f2a6b85dcc450dcd81
devanshu <devanshu.sharma@ayasya.com>
2025-11-13 16:59:24 +0530

    fixed css

--- employee-offers/components/CardGrid/Card.tsx
+++ employee-offers/components/CardGrid/Card.tsx
@@ -1,23 +1,24 @@
-import React from 'react'
-interface cardProps{
-    title:string;
-    subTitle:string;
-    bgImage:string;
-    width?:string;
+import React from "react";
+interface cardProps {
+  title: string;
+  subTitle: string;
+  bgImage: string;
+  width?: string | number;
 }
-const Card = ({title,subTitle,bgImage}:cardProps) => {
+const Card = ({ title, subTitle, bgImage, width }: cardProps) => {
   return (
-    <div className="relative w-full ">
-        <img src={bgImage}
-         className="w-full h-auto object-cover rounded-lg"  />
-    
-   <div className='absolute top-0 left-0 w-full h-full p-[30px]'>
-    
-    <h4 className='text-[26px] leading-[30px] text-[#1f1f1f] mb-4'>{title}</h4>
-    <p className={`text-[#333333cc] text-4 w-[width]`}>{subTitle}</p>
-   </div>
-   </div>
-  )
-}
+    <div className="relative w-full h-full">
+      <img src={bgImage} className=" rounded-3xl w-full h-full object-fill" />
+      <div className="absolute top-0 left-0 w-full h-full p-[30px]">
+        <h4 className="text-[26px] leading-[30px] text-[#1f1f1f] mb-4">
+          {title}
+        </h4>
+        <p className={`text-[#333333cc] text-4`} style={{ width: width }}>
+          {subTitle}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
---
+++ employee-offers/components/DiscoverCard/Banner/AdvantageBanner.tsx
@@ -0,0 +1,12 @@
+const AdvantageBanner = () => {
+  return (
+        <div className='w-full'>
+          <div className='w-[385px] lg:w-[305px] xl:w-[458px]'>
+            <img className='h-auto mb-3.5' src="/images/abg-heading-img.png" alt="Banner Image" />
+            <p className='text-xs leading-3.5 lg:text-sm lg:leading-4 xl:text-base font-semibold text-gray-600'>Aditya Birla Capital&apos;s range of financial products and services with offers Curated specially for you</p>
+          </div>
+        </div>
+  )
+}
+
+export default AdvantageBanner
