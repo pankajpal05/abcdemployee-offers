dac8d9b4cb57501b0329395223df01cd76802017
Abhishek Rajput <168184952+abhiayasya@users.noreply.github.com>
2025-11-17 12:31:50 +0530

    Refactor code structure for improved readability and maintainability

---
+++ employee-offers/app/campaign/thankyou/page.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import MaxwidthContainer from "../../../components/MaxWidthContainer/MaxWidthContainer";
+import Header from "../../../components/Header/Header";
+import FooterNote from "../../../components/FooterNote/FooterNote";
+
+const Thankyou = () => {
+  return (
+    <div>
+      <Header headerNote={false} />
+      <MaxwidthContainer extraClasses="w-full px-4 md:!px-10 2xl:!px-0 md:!pb-8 md:!pt-[90px] ">
+        <div className=" grid grid-cols-1 lg:rounded-4xl rounded-xl lg:grid-cols-2 xl:gap-[100px] items-center bg-[#70b86533] md:px-12">
+          {/* LEFT SIDE */}
+          <div className="flex flex-col items-center justify-center p-[15px]">
+            <img
+              src="/gif/successfull_tick.gif"
+              className="lg:max-w-[200px] max-w-[120px] w-full"
+              alt="Success"
+            />
+
+            <h3 className="sm:text-2xl text-lg font-semibold leading-7 text-center mb-4 text-[#1f1f1fcc]">
+              Thank you for sharing your responses. Our Regional
+              <br className="lg:hidden max-sm:hidden"/> Manager will
+              contact you soon.
+            </h3>
+
+            <div className=" xl:mt-[72px]">
+              <p className="text-[#111111cc] max-sm:text-sm text-lg mb-6 leading-[22px] text-center">
+                From{" "}
+                <i>
+                  smart investments to insurance, seamless payments to instant
+                  loans,
+                  <br className="lg:hidden max-sm:hidden"/>
+                </i>
+                <i>and track-your-life tools</i> – All In One Convenient App.
+              </p>
+
+              {/* BUTTON GROUP */}
+              <div className="flex gap-4 max-sm:flex-col justify-center items-center">
+                <a
+                  href="https://www.adityabirlacapital.com/?utm_source=abg_employee&utm_medium=organic&utm_campaign=microsite"
+                  target="_blank"
+                  className="bg-[#c91429] uppercase text-white max-sm:px-14 lg:px-6 xl:px-10 px-12 md:px-6 py-3 rounded-full font-medium hover:bg-[#c91429] transition"
+                >
+                  Explore Our Website
+                </a>
+
+                <a
+                  href="https://adityabirlacapitaldigital.onelink.me/vYLv/5lc79n1l"
+                  target="_blank"
+                  className="border-2 uppercase border-[#c91429] text-[#c91429] lg:px-6 xl:px-10 px-12 md:px-6 py-3 rounded-full font-medium hover:bg-red-50 transition flex items-center gap-2"
+                >
+                  Download App
+                  <span className="store-span flex gap-2 items-center">
+                    <img
+                      src="/images/ty-play-store.svg"
+                      alt="Play Store"
+                      className="w-6"
+                    />
+                    <img
+                      src="/images/ty-app-store.svg"
+                      alt="App Store"
+                      className="w-6"
+                    />
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* RIGHT SIDE */}
+          <div className="flex justify-center items-center">
+            <div className="relative w-full flex justify-center items-center">
+              <picture>
+                <source
+                  media="(min-width:991px)"
+                  srcSet="/images/thankyouApp-img.png"
+                />
+                <img
+                  src="/images/thankyouApp-img-mob.svg"
+                  alt="Thank You"
+                  className="w-full max-w-[535px] h-auto object-contain"
+                />
+              </picture>
+
+              {/* QR CODE BOX – only visible on md+ */}
+              {/* <div className="absolute bottom-0 z-50"> */}
+              <div className="hidden absolute max-w-50 w-full bottom-30 right-0 lg:flex justify-center items-center mt-6">
+                <img
+                  src="/images/downloadApp-textimg.png"
+                  alt="Download App"
+                  className="max-w-40 w-full mb-4 absolute"
+                />
+
+                <div className="absolute">
+                  <img
+                    src="/images/abg_employee_offers_microsite.png"
+                    alt="QR Code"
+                    className="max-w-20 max-h-20 w-full object-contain"
+                  />
+                </div>
+              </div>
+              {/* </div> */}
+            </div>
+          </div>
+        </div>
+      </MaxwidthContainer>
+      <FooterNote />
+    </div>
+  );
+};
+
+export default Thankyou;
--- employee-offers/components/Header/Header.tsx
+++ employee-offers/components/Header/Header.tsx
@@ -1,16 +1,22 @@
 "use client"
-import{ useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-const Header = () => {
+const Header = ({ headerNote = true }) => {
   const [showTop, setShowTop] = useState(true);
   const [logoScale, setLogoScale] = useState(1);
   const [lastScrollY, setLastScrollY] = useState(0);
 
   useEffect(() => {
+    if (!headerNote) {
+      // 💡 If headerNote is false → no animation
+      setShowTop(true);
+      setLogoScale(1);
+      return;
+    }
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
 
-      
+
       if (currentScrollY > lastScrollY) {
         // Scrolling up
         setShowTop(false);
@@ -33,35 +39,38 @@ const Header = () => {
       className="fixed top-0 left-0 right-0 bg-white shadow-sm z-50 transition-all duration-300"
     >
       <div className="flex items-center gap-5 justify-between">
-       
+
         <div
-          className="transition-all duration-300 pl-5"
-          style={{ transform: `scale(${logoScale})`, transformOrigin: "left center" , paddingTop: showTop ? '20px' : '0px'}}
+          className="transition-all duration-300 lg:pl-16 pl-[22px]"
+          style={{ transform: `scale(${logoScale})`, transformOrigin: "left center", paddingTop: showTop ? `${headerNote ? '20px' : "10px"}` : '0px' ,paddingBottom: showTop ? `${headerNote ? '0px' : "10px"}` : '0px' }}
         >
           <img src="/images/campaign_new_logo.png" alt="Logo" className="max-h-12 w-auto" />
         </div>
 
         <div className="w-full">
-       
-          <div
-           className={`flex justify-end items-center gap-10 transition-all ease-in-out duration-500 h-[30px]  ${showTop ? "opacity-100 max-h-[40px]" : "opacity-0 max-h-0"}`}
-          >
-            <p className="max-md:hidden text-[#2A2E34] text-sm">
-              Aditya Birla Capital Digital Limited
-            </p>
-            <a
-              href="tel:08035092882"
-              className="text-[#CA1F34] text-center text-sm font-bold max-md:text-xs  flex items-center gap-1 pr-2.5 lg:pr-10"
+          {
+            headerNote &&
+            <div
+              className={`flex justify-end items-center gap-10 transition-all ease-in-out duration-500 h-[30px]  ${showTop ? "opacity-100 max-h-[40px]" : "opacity-0 max-h-0"}`}
             >
-              <img src="/images/phone-call.png" alt="Phone Icon" />
-              080 350 92882
-            </a>
-          </div>
+              <p className="max-md:hidden text-[#2A2E34] text-sm">
+                Aditya Birla Capital Digital Limited
+              </p>
+              <a
+                href="tel:08035092882"
+                className="text-[#CA1F34] text-center text-sm font-bold max-md:text-xs  flex items-center gap-1 pr-2.5 lg:pr-10"
+              >
+                <img src="/images/phone-call.png" alt="Phone Icon" />
+                080 350 92882
+              </a>
+            </div>
 
-      
-          <div className="bg-[#CA1F34] px-10 h-16 rounded-bl-2xl items-center text-white flex justify-end">
-            <p className="md:hidden text-sm">Aditya Birla Capital Digital Limited</p>
-           
+          }
+
+
+          <div className={`bg-[#CA1F34] px-10 h-16 rounded-bl-2xl items-center text-white flex ${headerNote?"justify-end":"justify-start"}`}>
+            <p className={`${headerNote?"md:hidden text-sm":"text-lg font-semibold"}`}>Aditya Birla Capital Digital Limited</p>
+
           </div>
         </div>
       </div>
